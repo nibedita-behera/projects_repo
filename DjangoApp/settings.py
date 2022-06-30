d5c242a606b1e0f9db60a8260208b8cdf0ee6714
nibedita-behera <57294142+nibedita-behera@users.noreply.github.com>
2022-07-01 00:21:48 +0530

    commit04

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'DjangoApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,9 +121,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'digitalbudget/static')]
+# STATICFILES_DIRS=[os.path.join(BASE_DIR,'digitalbudget/static')]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
